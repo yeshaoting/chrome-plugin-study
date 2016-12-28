9c1a046f098957a1ca6ad7dc425b6ef7c21f0839
叶绍亭 <yeshaoting@meituan.com>
2016-12-28 17:08:45 +0800

    plugin

---
+++ start/js/main.js
@@ -0,0 +1,84 @@
+var url = "http://sentence.iciba.com/index.php";
+
+window.onload = function() {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      title: "",
+      content: "",
+      note: "",
+      translation: "",
+      picture: "",
+      picture2: "",
+      picture3: "",
+      caption: "每日一句",
+      tts: "",
+    },
+    ready: function() {
+      this.get('2016-11-12');
+    },
+    methods: {
+      get: function(title) {
+        var now = moment();
+        var params = {
+          "c": "dailysentence",
+          "m": "getdetail",
+          "title": title ? title : now.format('YYYY-MM-DD')
+        }
+
+        var options = {
+          params: params
+        };
+        if (chrome.extension) {
+          console.debug("in chrome extenstion");
+          result = this.$http.get(url, options);
+        } else {
+          console.debug("not in chrome extenstion");
+          result = this.$http.jsonp(url, options);
+        }
+
+        result.then((response) => {
+          this.apply(response.data);
+        }).catch(function(response) {
+          console.log(JSON.stringify(response))
+        });
+      },
+      request: function(title) {
+        var vm = this;
+        var now = moment();
+        var params = {
+          "c": "dailysentence",
+          "m": "getdetail",
+          "title": title ? title : now.format('YYYY-MM-DD')
+        }
+
+        $.ajax({
+          url: url,
+          dataType: 'jsonp',
+          data: $.param(params, true),
+          jsonp: 'callback',
+          timeout: 3000,
+          success: function(response) {
+            vm.apply(response);
+          }
+        });
+      },
+      apply: function(data) {
+        //console.debug("data: %s", JSON.stringify(data));
+        console.info("apply data into page~");
+        this.$set('title', data.title);
+        this.$set('content', data.content);
+        this.$set('note', data.note);
+        this.$set('translation', data.translation);
+        this.$set('picture', data.picture);
+        this.$set('picture2', data.picture2);
+        this.$set('picture3', data.picture3);
+        this.$set('caption', data.caption);
+        this.$set('tts', data.tts);
+      }
+    }
+  });
+
+  placeholder.render();
+
+}
